8c5a11c5f533251e9dbc1ddf6bcf043683b51f6d
Md Adil Farhan <imfarhan574@gmail.com>
2025-07-25 00:43:27 +0530

    Week 6

---
+++ HandsOnWeek6/React/ReactJS-HOL 2/studentapp/src/Components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <h2>Welcome to the About page of Student Management Portal</h2>
+    );
+}
+
+export default About;
---
+++ HandsOnWeek6/React/ReactJS-HOL 3/scorecalculatorapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore 
+        Name={"Adil"}
+        School={"Md Jan High School"}
+        total={284}
+        goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
